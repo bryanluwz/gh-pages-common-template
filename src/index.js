299764ebd2afb13b50d12b991579f7491e888d8a
Bryan <bryanlu.my@gmail.com>
2023-05-20 11:14:44 +0800

    added basename to hash router

--- src/index.js
+++ src/index.js
@@ -2,14 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { RouterProvider, createHashRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const router = createHashRouter([{
-	path: "/*",
-	element: <App />
-}]);
 
 root.render(
-	<RouterProvider router={router} />
+	<HashRouter basename={process.env.PUBLIC_URL}>
+		<App />
+	</HashRouter>
 );
